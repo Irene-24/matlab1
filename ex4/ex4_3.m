ebdf6d9064dc8082347383fad194c2157495dacf
john9384 <jogungbure@gmail.com>
2021-08-26 13:11:47 +0100

    Update ex4_3.m

--- ex4/ex4_3.m
+++ ex4/ex4_3.m
@@ -1,4 +1,4 @@
-f=imread(".\images\ns.jpg");
+f=imread(".\images\skeleton.jpg");
 
 F=fft2(f);
 
@@ -6,41 +6,3 @@ S=abs(F);
 
 imshow(S);
 
-
-f=im2double(f);
-F=fft2(f);
-S=fftshift(log(1+abs(F)));
-imshow(S,[]);
-
-h=fspecial('sobel');
-%disp(h);
-%disp(freqz2(h));
-
-PQ=paddedsize(size(f));
-H=freqz2(h,PQ(1),PQ(2));
-H1=ifftshift(H);
-
-imshow(abs(H),[]);
-figure,imshow(abs(H1),[]);
-
-gs=imfilter(f,h);
-
-gf=dftfilt(f,H1);
-
-imshow(gs,[]);
-figure,imshow(gf,[]);
-figure,imshow(abs(gf),[]);
-figure,imshow(abs(gf),[]);
-
-figure,imshow(abs(gf)>0.2*abs(max(gs(:))));
-figure,imshow(abs(gf)>0.2*abs(max(gf(:))));
-
-d=abs(gs-gf);
-disp(max(d(:)));
-disp(min(d(:)));
-
-
-[U,V]=dftuv(8,5);
-DSQ=U.^2+V.^2;
-disp(DSQ)
-disp(fftshift(DSQ)
